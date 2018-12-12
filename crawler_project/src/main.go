59e1d4ae272bd954ca7f86179075db1954075b12
DazzlingSun <you@example.com>
2018-12-12 17:23:52 +0800

    增加链家爬取工具并编写爬取城市信息

--- crawler_project/src/main.go
+++ crawler_project/src/main.go
@@ -1,9 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"utils"
+)
+
+const xLIANJIA_CITY_URL = "https://bj.lianjia.com/city/"
+const TEST_FILE_PATH = "C:\\Users\\admin\\Desktop\\crawler\\lianjia_city.html"
 
 func main() {
-	http.Client{
 
-	}
+	//node := utils.ReadNodeByHttp(xLIANJIA_CITY_URL)
+	//fmt.Println(node.FirstChild.NextSibling)
+	//bodyNode := utils.GetBodyNode(utils.GetHeadHtml(node))
+
+	//获取城市url数据结构
+	cityUrlList := &utils.CityUrlList{"https://fs.lianjia.com/city/", make([]utils.CityUrl, 0)}
+	cityUrlList.HtmlParse()
+	fmt.Println(cityUrlList)
+
+	//file := utils.ReadNodeByFile(TEST_FILE_PATH)
+
+	//fmt.Println(file.FirstChild)
+
+	//fmt.Println(utils.FindNodeByAtrr("class","city_selection_section", utils.GetBodyNode(file)))
+
 }
--- crawler_project/src/utils/htmlParse.go
+++ crawler_project/src/utils/htmlParse.go
@@ -1,22 +1,56 @@
 package utils
 
 import (
-	"os"
-	"io/ioutil"
 	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
+	"os"
 )
 
-func readHtmlFile(path string)(b []byte)  {
+func ReadNodeByFile(path string) *html.Node {
 	file, e := os.Open(path)
 	defer file.Close()
-	if e !=nil {
-		fmt.Println("html文件打开失败",e)
+	if e != nil {
+		fmt.Println("html文件打开失败", e)
 	}
-	bytes, e2 := ioutil.ReadAll(file)
-	if e2!=nil {
-		fmt.Printf("文件读取错误",e2)
+	node, err := html.Parse(file)
+	if err != nil {
+		fmt.Println("读取html内容失败", err)
 	}
-	return bytes
+	return node
 }
 
-func
+func ReadNodeByHttp(url string) *html.Node {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("获取http连接失败", resp)
+	}
+	defer resp.Body.Close()
+	node, err1 := html.Parse(resp.Body)
+	if err1 != nil {
+		fmt.Println("读取html内容失败", err)
+	}
+	return node
+}
+func FindNodeByAtrr(atrrName, attrValue string, node *html.Node) *html.Node {
+	if node == nil {
+		return nil
+	}
+	attributes := node.Attr
+	for _, attr := range attributes {
+		if attr.Key == atrrName && attr.Val == attrValue {
+			return node
+		}
+	}
+	for n := node.FirstChild; n != nil; n = n.NextSibling {
+		destNode := FindNodeByAtrr(atrrName, attrValue, n)
+		if destNode != nil {
+			return destNode
+		}
+	}
+	return nil
+}
+
+type Parse interface {
+	HtmlParse()
+}
